45f443a9e652e2b7e4963ff5016e962baf2147cc
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-02 14:05:33 +0200

    finalizza verifica se parola è palindroma

--- palindroma/script.js
+++ palindroma/script.js
@@ -2,3 +2,18 @@
 let userInput = prompt("Inserisci una parola");
 
 //verifica se palindroma
+let isPalindroma = false;
+for (let i = 0; i < userInput.length; i++){
+    if (userInput[i] === userInput[userInput.length-i-1]){
+        isPalindroma = true;
+        
+    }else {
+        isPalindroma = false;
+    }
+}
+
+if(isPalindroma === true) {
+    console.log("La parola è palindroma!");
+}else {
+    console.log("la parola non è palindroma!");
+}
