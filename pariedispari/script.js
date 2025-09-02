bcaa1bcf3eb9c7cfef10c8abca769d4d20e9c788
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-02 14:22:11 +0200

    inizializza inserimento dati utente

--- pariedispari/script.js
+++ pariedispari/script.js
@@ -1,5 +1,16 @@
 //scelta utente e inserimento numero
+let userChoice = prompt("scegli 1 per pari o 2 per dispari");
+while (userChoice != "pari" && userChoice != "dispari"){
+    console.log("inserisci una scelta valida")
+}
 
+let userNum = prompt("inserisci un numero da 1 a 5");
+while (userChoice < 1 && userChoice > 5){
+    console.log("inserisci una scelta valida")
+}
+console.log(userChoice, userNum);
 // genera e somma nuovo numero
 
-//
+//stabilisci se pari o dispari (funzione)
+
+//dichiara il vincitore
